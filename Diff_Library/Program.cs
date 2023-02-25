98a77268ba37de4f28120e4f78f7a0fae2f2c5df
maxpoint0118 <maxpoint0118@naver.com>
2023-02-25 16:11:13 +0900

    23.02.25 readme 추가, 리눅스 빌드 파일 추가

--- Diff_Library/Program.cs
+++ Diff_Library/Program.cs
@@ -4,25 +4,28 @@
     {
         static void Main(string[] args)
         {
-            if (args.Length < 2) return;
-
-            if (!File.Exists(args[0]))
+            //if (args.Length < 2) return;
+            Console.WriteLine("소스 파일 경로를 입력해주세요.");
+            string source_Path = Console.ReadLine();
+            if (!File.Exists(source_Path))
             {
-                Console.WriteLine("첫 번째 경로의 파일을 확인해주세요.");
+                Console.WriteLine("소스 파일의 경로를 확인해주세요.");
+                return;
+            }
+            
+            Console.WriteLine("타겟 파일 경로를 입력해주세요.");
+            string target_Path = Console.ReadLine();
+            if (!File.Exists(target_Path))
+            {
+                Console.WriteLine("타겟 파일읙 경로를 확인해주세요.");
                 return;
             }
 
-            if (!File.Exists(args[1]))
-            {
-                Console.WriteLine("두 번째 경로의 파일을 확인해주세요.");
-                return;
-            }
-
-            string sSource = System.IO.File.ReadAllText(args[0]);
-            string sTartget = System.IO.File.ReadAllText(args[1]);
-
-            diff_match_patch diff = new diff_match_patch(); // 객체 생성
-            var output = diff.diff_main(sSource, sTartget);      // 파일 비교
+            string source = System.IO.File.ReadAllText(source_Path);
+            string tartget = System.IO.File.ReadAllText(target_Path);
+           
+            diff_match_patch diff = new(); // 객체 생성            
+            var output = diff.diff_main(source, tartget);      // 파일 비교
             Console.WriteLine(string.Join("\n", output));   // 비교 내용 출력
             
         }
---
+++ Diff_Library/readme.txt
@@ -0,0 +1,13 @@
+﻿--윈도우
+1. CMD창을 실행 프로젝트의 Diff_Library/bin/Release/net7.0/  경로로 이동
+2. Diff_Library 입력 후 엔터
+3. 실행
+
+
+--리눅스
+1. 윈도우에서 CMD를 실행.
+2. CMD창에서 .csproj 파일의 경로(Diff_Library\)로 이동
+3. dotnet publish -r linux-x64
+4. 생성 완료된 <프로젝트>\linux-x64\publish 파일을 리눅스계열의 OS로 이동
+5. 리눅스 터미널에서 dotnet Diff_Library.dll 입력 후 엔터
+6. 실행
